cd9b261811aa1ce4ebc6d78985fd9258eb8bf96a
Charis <961295428@qq.com>
2019-03-08 20:03:07 +0800

    功能完善

--- paint.js
+++ paint.js
@@ -17,7 +17,7 @@
   var ratio = 4;
   // 画布矩阵
   var array = [];
-  // 画笔对绘板的相对坐标
+  // 画笔对绘板的相对坐标(0-200,0-100)
   var brushX = 0;
   var brushY = 0;
 
@@ -29,46 +29,47 @@
 
   // 鼠标经过时像素点变色
   $('#canvas').mousemove(function (e) {
-    brushX = Math.floor(e.pageX - $(this).offset().left); // 当前鼠标相对canvas的X坐标
-    brushY = Math.floor(e.pageY - $(this).offset().top); // 当前鼠标相对canvas的Y坐标
-    // 覆盖一个元素
-    $('#brush').css('top', brushY);
-    $('#brush').css('left', brushX);
+    brushX = Math.floor((e.pageX - $('.window').offset().left - 10) / ratio); // 当前鼠标相对canvas的X坐标
+    brushY = Math.floor((e.pageY - $('.window').offset().top - 10) / ratio); // 当前鼠标相对canvas的Y坐标
+    // 覆盖一个元素 不能出边界
+    if (brushX < 0 || brushX > 199) return;
+    if (brushY < 0 || brushY > 99) return;
+    $('#brush').css('top', brushY * ratio);
+    $('#brush').css('left', brushX * ratio);
   });
 
   // 点击改变画布颜色
   $('#brush').click(function (e) {
+    if (brushX < 0 || brushX > 199) return;
+    if (brushY < 0 || brushY > 99) return;
     // 若颜色与原来不一致则改变
-    var x = Math.floor(brushX / ratio);
-    var y = Math.floor(brushY / ratio);
-    if (array[y][x] !== brushColor) {
+    if (array[brushY][brushX] !== brushColor) {
       ctx.fillStyle = colors[brushColor];
-      ctx.fillRect(x * ratio, y * ratio, ratio, ratio);
+      ctx.fillRect(brushX * ratio, brushY * ratio, ratio, ratio);
       // 需要有3s间隔
-      // ajax.post('/paint', {
-      //   x: x,
-      //   y: y,
-      //   color: brushColor
-      // }).then(function (res) {
+      ajax.post('/paint', {
+        'x': brushX,
+        'y': brushY,
+        'color': parseInt(brushColor)
+      }).then(function (res) {
 
-      // });
+      });
     }
   });
 
-  // 滚轮事件 delta 1 放大 -1 缩小
-  $('#canvas').mousewheel(function (e, delta, deltaX, deltaY) {
-    if (delta > 0) {
-      // 设置缩放中心点
-      $(this).css('transform-origin', brushX / (200 * ratio) + ' ' + brushY / (100 * ratio));
-      $(this).css('-webkit-transform-origin', brushX / (200 * ratio) + ' ' + brushY / (100 * ratio));
-      // $(this).css('transform', 'scale(' + ratio + ')');
-    } else {
-      $(this).css('transform', 'scale(1)');
-    }
+  // 滚轮事件 delta 1 放大 -1 缩小 每次放大两倍
+  $('#brush,#canvas').mousewheel(function (e, delta, deltaX, deltaY) {
+    // 禁止页面滚动
+    $('body').css('overflow', 'hidden');
+    // 缩放中心点为当前画笔位置
+    $('#canvas').css('transform-origin', brushX + ' ' + brushY);
+    $('#canvas').css('-webkit-transform-origin', brushX + ' ' + brushY);
+    ratio = delta > 0 ? ratio * 2 : ratio / 2
+    $('#canvas').css('transform', 'scale(' + ratio / 4 * 2 + ')');
+    // 改变像素大小
+    $('#brush').css('width', ratio + 'px');
+    $('#brush').css('height', ratio + 'px');
   });
-
-  // 画布拖动
-
 
   // 初始化绘板
   function init() {
